b08a26a26e34a45421f652f342116ab6e7068407
Asuna <1259311026@qq.com>
2025-03-08 17:31:19 +0800

    井字棋游戏(未完成)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
+
+
 import { useState } from "react";
 
-const MyButton = ({cnt,handleClick}) => {
-
-  return(
-    <button onClick={handleClick}>
-      点了{cnt}次
-    </button>
-  );
-
-};
+const Square = ({value,onSquareClick}) => {
+  
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+}
 
 function App() {
-  const [cnt,setCnt] = useState(0);
+  const [xIsNext,setXisNext] = useState(true);
+  const [squares,setSquares] = useState(Array(9).fill(null));
 
-  const handleClick = () => {
-    setCnt(cnt+1);
-  }
+  const handleClick = (i) => {
+    if(squares[i]) return;
+  const nextSquares = squares.slice();
+  nextSquares[i] = xIsNext ? 'X' : 'O';
+  setXisNext(!xIsNext);
+  setSquares(nextSquares);
+}
+
   return (
-    <div className="App">
-      <h3>共同更新的计数器</h3>
-     <MyButton cnt={cnt} handleClick={handleClick}/>
-     <br />
-     <MyButton cnt={cnt} handleClick={handleClick}/>   
+      <div className="App">
+      <div className="board-row">
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
     </div>
   );
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for(let i = 0 ; i < lines.length ; i++) {
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // React必要的两个核心包
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./style.css";
 
 import App from "./App";
 
