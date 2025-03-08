edf0c025a7e96316ea31db361d3d07db84f5a74f
Asuna <1259311026@qq.com>
2025-03-08 13:23:18 +0800

    first tmp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+// 项目的根组件
+// useState是实现一个计数器
+import { useState } from "react";
 function App() {
+  // 1.调用useState添加一个状态变量
+  // count 状态变量
+  // setCount 修改状态变量的方法
+  const [count, setCount] = useState(0);
+
+  // 2. 点击事件回调
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React必要的两个核心包
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from "./App";
+
+// 把App根组件渲染到id为root的dom节点上
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
